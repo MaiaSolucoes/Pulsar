7447ab49de874fca98bcff9509d5b222d56fe693
Cicero <cicero@maiasolucoes.com.br>
2013-03-06 23:17:45 -0300

    pulsar x www

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -2,50 +2,52 @@
 
 class Auth_Controller extends Base_Controller {
 
-	public $restful = true;
+    public $restful = true;
 
     private static $cache_timeout = 10;
 
-	public function get_index() {
-		return View::make('home.index');
-	}
+    public function get_index() {
+        return View::make('home.index');
+    }
 
-	public function get_login() {
+    public function get_login() {
 
-		$cache_id = 'auth_'.Input::get('username');
+        $cache_id = 'auth_'.Input::get('username');
+
+        Cache::put('auth','verdade',10);
+        $auth = Cache::get('auth');
 
         $credentials = array(
-			'username' => Input::get('username'),
-			'password' => Input::get('password'),
-		);
+            'username' => Input::get('username'),
+            'password' => Input::get('password'),
+        );
 
         if(empty($credentials['username']) or empty($credentials['password'])){
-            return Response::json(null, 412);
+            return Response::json('embranco', 412);
         } else {
-           $response = Cache::remember(
+            $response = Cache::remember(
                 $cache_id,
-                function() use($credentials) {
-					return Auth::attempt($credentials)
-						? Response::json(Session::token(), 200)
-						: Response::json(null, 404);
-				},
+                function() use($credentials,$auth) {
+                    if(Auth::attempt($credentials)){
+                        return Response::json( $auth, 200);
+
+                    }else{
+                        return Response::json('auth nao criado', 404);
+                    }
+                },
                 self::$cache_timeout
             );
 
-			/**
-			 * If Auth::attempt() fails, what happens with its Session::token()?
-			 * This code fix it in case of 404 response.
-			 **/
-			if($response->status() == 404) {
-				Cache::forget($cache_id);
-			}
-			return $response;
+            if($response->status() == 404) {
+                Cache::forget($cache_id);
+            }
+            return $response;
 
         }
-	}
+    }
 
-	public function get_logout() {
-		Auth::logout();
-		return View::make('home.logout');
-	}
+    public function get_logout() {
+        Auth::logout();
+        return View::make('home.logout');
+    }
 }
