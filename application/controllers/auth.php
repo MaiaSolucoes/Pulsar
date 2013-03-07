5fa6160f7369ee4e3f7a875ce7838d9574bba192
Ricardo Maia <maiams@msn.com>
2013-03-06 23:44:16 -0300

    fix: problema grave de autenticacao com bypass de senha

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -13,30 +13,24 @@ class Auth_Controller extends Base_Controller {
     public function get_login() {
 
         $cache_id = 'auth_'.Input::get('username');
-
-        Cache::put('auth','verdade',10);
-        $auth = Cache::get('auth');
-
         $credentials = array(
             'username' => Input::get('username'),
             'password' => Input::get('password'),
         );
 
-        if(empty($credentials['username']) or empty($credentials['password'])){
+        if(empty($credentials['username']) or empty($credentials['password'])) {
             return Response::json('embranco', 412);
         } else {
-            $response = Cache::remember(
-                $cache_id,
-                function() use($credentials,$auth) {
-                    if(Auth::attempt($credentials)){
-                        return Response::json( $auth, 200);
-
-                    }else{
-                        return Response::json('auth nao criado', 404);
-                    }
-                },
-                self::$cache_timeout
-            );
+			if(Auth::attempt($credentials)) {
+				$response = Cache::remember(
+					$cache_id,
+					function() { return Session::token(); },
+					self::$cache_timeout
+				);
+				return Response::json($response, 200);
+			} else {
+				return Response::json(null, 404);
+			}
 
             if($response->status() == 404) {
                 Cache::forget($cache_id);
