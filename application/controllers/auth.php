aee9c0baa68617ada80a2bd3106a50d064bcf364
Cicero <cicero@maiasolucoes.com.br>
2013-03-15 21:23:46 -0300

    autenticacao pronta,(login,logout e check)

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -7,7 +7,9 @@ class Auth_Controller extends Base_Controller {
     private static $cache_timeout = 1;
 
     public function get_index() {
+
         return View::make('home.index');
+
     }
 
     public function get_login() {
@@ -16,28 +18,40 @@ class Auth_Controller extends Base_Controller {
             'username' => Input::get('username'),
             'password' => Input::get('password'),
         );
+
         $time = Auth_Controller::$cache_timeout;
 
         if(empty($credentials['username']) or empty($credentials['password'])){
+
             return Response::json(null, 412);
+
         } else {
+
             $cache_id = Input::get('username');
             $response = Cache::remember(
                 $cache_id,
                 function() use($credentials) {
 
                     if(Auth::attempt($credentials)) {
+
                         return Session::token();
+
                     } else {
+
                         return null;
                     }
                 },
                 $time
             );
+
             if($response == null) {
+
                 return $this->get_logout();
+
             }
+
             return Response::json(array($cache_id => $response),200);
+
         }
     }
 
@@ -77,15 +91,12 @@ class Auth_Controller extends Base_Controller {
         $user = Input::get('username');
 
         $token = Input::get('token');
-        //return Cache::has($user) ? 'sim' : 'nao';
-
-        //return $cache_token = Cache::get($user).'get';
 
         $response = Response::json(false,200);
 
         $cache_token = false;
 
-        if(empty($user) or empty($token)){
+        if(is_null($user) or is_null($token)){
 
             return $response;
 
@@ -93,8 +104,11 @@ class Auth_Controller extends Base_Controller {
 
             Cache::has($user) ? $cache_token = Cache::get($user) : null;
 
-            $token == $cache_token ? $response = Response::json(true,200) : null;
+            if($token == $cache_token){
 
+                $response = Response::json(true,200);
+                Cache::put($user,$token,1);
+            }
             return $response;
         }
 
